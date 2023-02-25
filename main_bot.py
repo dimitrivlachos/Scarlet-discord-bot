5d7d05b55b42509ff4c5fb9193f9e51f18f29601
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-02-25 02:43:18 +0000

    Added comments

--- main_bot.py
+++ main_bot.py
@@ -1,26 +1,34 @@
 '''
-Simple discord bot that responds to commands
+Basic discord bot that responds to messages with "Hello, World!"
 '''
 
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
-import asyncio
-import time
 
 intents = discord.Intents.default()
 intents.members = True
-client = discord.Client(intents=intents)
-
-#client = commands.Bot(command_prefix = "!")
-
-@client.event
-
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+client = commands.Bot(command_prefix='!', intents=intents)
 
 # Reads token from binary file
 with open("config/token.bin", "rb") as binary_file:
     token = binary_file.read().decode()
 
+# Prints a message to the console when the bot is ready
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+# Responds to messages with "Hello, World!"
+@client.event
+async def on_message(message):
+    # Ignore messages sent by the bot itself
+    if message.author == client.user:
+        return
+
+    response = 'Hello, World!'
+
+    # Send the response to the Discord channel
+    await message.channel.send(response)
+
+# Run the bot
 client.run(token)
