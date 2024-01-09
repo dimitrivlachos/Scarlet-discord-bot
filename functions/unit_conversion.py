c56433c76bb1b22bf0b6c3d65b68e80bc0cf42e9
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2024-01-09 18:23:35 +0000

    Added basic unit conversions

---
+++ functions/unit_conversion.py
@@ -0,0 +1,11 @@
+def kelvin_to_celsius(temp):
+    return round(temp - 273.15)
+
+def kelvin_to_fahrenheit(temp):
+    return round((temp - 273.15) * 9/5 + 32)
+
+def metres_per_sec_to_kmh(speed):
+    return round(speed * 3.6)
+
+def metres_per_sec_to_mph(speed):
+    return round(speed * 2.237)
