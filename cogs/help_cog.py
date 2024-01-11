6405e5a119b0c6172bcb1a1c46050d6e890571fa
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2024-01-11 16:54:49 +0000

    Improved help embeds

--- cogs/help_cog.py
+++ cogs/help_cog.py
@@ -6,9 +6,10 @@ from utility.tokens import BISCUIT_ID
 class help_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.help_embed = self.set_embed()
+        self.set_help_embed()
+        self.set_music_embed()
 
-    def set_embed(self):
+    def set_help_embed(self):
         help_description = f"""
         Hi! I am a bot created by <@!{BISCUIT_ID}>.
         I can do a few things, but I am still in development, so be kind!
@@ -35,27 +36,49 @@ class help_cog(commands.Cog):
 
         # Music commands
         music_commands = f"""
-        {self.bot.command_prefix}join - Joins the voice channel you are in
-        {self.bot.command_prefix}play [query] - Plays / adds a song to the queue
-        {self.bot.command_prefix}add [query] - Plays / adds a song to the queue
-        {self.bot.command_prefix}pause - Pauses / Resumes the current song
-        {self.bot.command_prefix}resume - Resumes the current song
-        {self.bot.command_prefix}skip - Skips the current song
-        {self.bot.command_prefix}queue [depth] - Displays the queue (depth optional)
-        {self.bot.command_prefix}clear - Clears the current queue
-        {self.bot.command_prefix}stop - Stops the current song, clears the queue and leaves
-        {self.bot.command_prefix}remove [query] - Removes the last song from the queue
-        {self.bot.command_prefix}move [song] [position] - Moves a song to a new position in the queue
-        {self.bot.command_prefix}shuffle - Shuffles the queue
+        `{self.bot.command_prefix}join` - Joins the voice channel you are in
+        `{self.bot.command_prefix}play` [query] - Plays / adds a song to the queue
+        `{self.bot.command_prefix}add` [query] - Plays / adds a song to the queue
+        `{self.bot.command_prefix}pause` - Pauses / Resumes the current song
+        `{self.bot.command_prefix}resume` - Resumes the current song
+        `{self.bot.command_prefix}skip` - Skips the current song
+        `{self.bot.command_prefix}queue` [depth] - Displays the queue (depth optional)
+        `{self.bot.command_prefix}clear` - Clears the current queue
+        `{self.bot.command_prefix}stop` - Stops the current song, clears the queue and leaves
+        `{self.bot.command_prefix}remove` [query] - Removes the specified song from the queue
+        `{self.bot.command_prefix}move` [song] [position] - Moves a song to a new position in the queue
+        `{self.bot.command_prefix}shuffle` - Shuffles the queue
         """
         self.help_embed.add_field(name="Music commands", value=music_commands, inline=False)
 
         # Add the help command to the embed
-        self.help_embed.add_field(name="!help", value="Displays this message", inline=False)
+        self.help_embed.add_field(name=f"{self.bot.command_prefix}help", value=f"Displays this message. Or `{self.bot.command_prefix}help music` (`{self.bot.command_prefix}help m`) to show more detail on the music commands", inline=False)
+    
+    def set_music_embed(self):
+        music_description = f"""
+        Here are the commands you can use to control the music:
+        """
+        self.music_embed = discord.Embed(title="Music help", description=music_description, color=0x00ff00)
 
-        return self.help_embed
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}join ({self.bot.command_prefix}j)", value="Joins the voice channel you are in", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}play [query] ({self.bot.command_prefix}p {self.bot.command_prefix}playing {self.bot.command_prefix}a {self.bot.command_prefix}add)", value="Plays / adds a song to the queue", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}add [query] ({self.bot.command_prefix}p {self.bot.command_prefix}playing {self.bot.command_prefix}a {self.bot.command_prefix}add))", value="Plays / adds a song to the queue", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}pause", value="Pauses / Resumes the current song", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}resume ({self.bot.command_prefix}r)", value="Resumes the current song", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}skip ({self.bot.command_prefix}s)", value="Skips the current song", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}queue [depth] ({self.bot.command_prefix}q)", value="Displays the queue (depth optional)", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}clear ({self.bot.command_prefix}c {self.bot.command_prefix}bin {self.bot.command_prefix}empty)", value="Clears the current queue", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}stop ({self.bot.command_prefix}disconnect {self.bot.command_prefix}l {self.bot.command_prefix}d {self.bot.command_prefix}dc)", value="Stops the current song, clears the queue and leaves", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}remove [query] ({self.bot.command_prefix}rm)", value="Removes the specified song from the queue", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}move [song] [position] ({self.bot.command_prefix}mv)", value="Moves a song to a new position in the queue", inline=False)
+        self.music_embed.add_field(name=f"{self.bot.command_prefix}shuffle ({self.bot.command_prefix}sh)", value="Shuffles the queue", inline=False)
     
     @commands.command(name='help', help="Displays all of my functions")
-    async def help(self, ctx):
-        logger.info(f"Sending help message to <{ctx.author}>")
-        await ctx.send(embed=self.help_embed)
+    async def help(self, ctx, *args):
+        query = " ".join(args)
+        if query == "music" or query == "m":
+            logger.info(f"Sending music help message to <{ctx.author}>")
+            await ctx.send(embed=self.music_embed)
+        else:
+            logger.info(f"Sending help message to <{ctx.author}>")
+            await ctx.send(embed=self.help_embed)
